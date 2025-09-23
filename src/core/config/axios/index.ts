e1ea46ad33dccbcc19faeacfad92c351cc0f3735
edwinmghdez <edwin.mghdez@gmail.com>
2025-09-23 11:18:28 -0600

    fix: Remove console logs

--- src/core/config/axios/index.ts
+++ src/core/config/axios/index.ts
@@ -10,23 +10,9 @@ const client = axios.create({
 
 client.interceptors.response.use(
   (response) => {
-    console.log('Interceptor - Response:', {
-      status: response.status,
-      url: response.config.url,
-      headers: response.headers,
-      response: response.data,
-    })
     return response.data
   },
   async (error) => {
-    // Review error response
-    console.log('Interceptor - Error:', {
-      status: error.response?.status,
-      url: error.config?.url,
-      message: error.message,
-      response: error.response?.data,
-    })
-
     return Promise.reject(error)
   },
 )
